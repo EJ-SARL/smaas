e46efce9b600a5f2fab9afce518423459d0a372a
thierry elia <eliamaisongo055@gmail.com>
2023-02-21 12:22:53 +0100

    ajoute les composants 2 et 3 dans content

--- home/composant/content.php
+++ home/composant/content.php
@@ -22,11 +22,49 @@
                     require_once('composant/client/supprimer/api/delete_user.php'); 
                 }
 
-            //Composant2 : recuperer la reponse de la demande Itineraire
+                if($demande=='ajouter_agent')
+                {
+                    //Composant2 : recuperer la reponse de la demande
+                    require_once('composant/agent/ajout/api/post.php'); 
+                }
+            else if($demande=='modifier_agent')
+                {
+                    
+                     //Composant2 : recuperer la reponse de la demande
+                    require_once('composant/agent/modifier/api/put_user.php'); 
+                }
+            else if($demande=='supprimer_agent')
+                {
+                    
+                     //Composant2 : recuperer la reponse de la demande
+                    require_once('composant/agent/supprimer/api/delete_user.php'); 
+                }
+
+
+                if($demande=='ajouter_bus')
+                {
+                    //Composant3 : recuperer la reponse de la demande
+                    require_once('composant/bus/ajout/api/post.php'); 
+                }
+            else if($demande=='modifier_bus')
+                {
+                    
+                     //Composant13: recuperer la reponse de la demande
+                    require_once('composant/bus/modifier/api/put_user.php'); 
+                }
+            else if($demande=='supprimer_bus')
+                {
+                    
+                     //Composant3 : recuperer la reponse de la demande
+                    require_once('composant/agent/supprimer/api/delete_user.php'); 
+                }
+
+
+            //Composant4 : recuperer la reponse de la demande Itineraire
 
             else if($demande=='ajouter_itineraire')
                 {
-                    //Composant1 : recuperer la reponse de la demande
+                    //Composant4 : recuperer la reponse de la demande
                     require_once('composant/itineraire/ajout/api/post.php'); 
                 }
         
@@ -72,11 +110,87 @@
                     require_once('composant/client/recuperer/api/get_user.php'); 
                 }
 
-                 //Composant2 :Ituneraire
+
+                if($page=='ajouter_agent')
+                {
+                    //Composant2 : recuperer la reponse de la demande
+                    require_once('composant/agent/ajout/ihm/demande_creation.php'); 
+                }
+            else  if($page=='modifier_agents')
+                {
+                    //Composant2 : recuperer la reponse de la demande
+                    require_once('composant/agent/modifier/api/get_users.php'); 
+                }
+            else  if($page=='modifier_agent')
+                {
+                    //Composant2 : recuperer la reponse de la demande
+                    require_once('composant/agent/modifier/api/get_user.php'); 
+                }
+            else  if($page=='supprimer_agents')
+                {
+                    //Composant1 : recuperer la reponse de la demande
+                    require_once('composant/agent/supprimer/api/get_users.php'); 
+                }
+            else  if($page=='supprimer_agent')
+                {
+                    //Composant2 : recuperer la reponse de la demande
+                    require_once('composant/agent/supprimer/api/get_user.php'); 
+                }
+
+            else  if($page=='recuperer_agents')
+                {
+                    //Composant2 : recuperer la reponse de la demande
+                    require_once('composant/agent/recuperer/api/get_users.php'); 
+                }
+            else  if($page=='recuperer_agent')
+                {
+                    //Composant2 : recuperer la reponse de la demande
+                    require_once('composant/agent/recuperer/api/get_user.php'); 
+                }
+
+
+                if($page=='ajouter_bus')
+                {
+                    //Composant1 : recuperer la reponse de la demande
+                    require_once('composant/bus/ajout/ihm/demande_creation.php'); 
+                }
+            else  if($page=='modifier_buss')
+                {
+                    //Composant1 : recuperer la reponse de la demande
+                    require_once('composant/bus/modifier/api/get_users.php'); 
+                }
+            else  if($page=='modifier_bus')
+                {
+                    //Composant1 : recuperer la reponse de la demande
+                    require_once('composant/bus/modifier/api/get_user.php'); 
+                }
+            else  if($page=='supprimer_buss')
+                {
+                    //Composant1 : recuperer la reponse de la demande
+                    require_once('composant/bus/supprimer/api/get_users.php'); 
+                }
+            else  if($page=='supprimer_bus')
+                {
+                    //Composant1 : recuperer la reponse de la demande
+                    require_once('composant/bus/supprimer/api/get_user.php'); 
+                }
+
+            else  if($page=='recuperer_buss')
+                {
+                    //Composant1 : recuperer la reponse de la demande
+                    require_once('composant/bus/recuperer/api/get_users.php'); 
+                }
+            else  if($page=='recuperer_bus')
+                {
+                    //Composant1 : recuperer la reponse de la demande
+                    require_once('composant/bus/recuperer/api/get_user.php'); 
+                }
+
+                 //Composant :Ituneraire
            
             else  if($page=='ajouter_itineraire')
                 {
-                    //Composant1 : recuperer la reponse de la demande
+                    //Composant : recuperer la reponse de la demande
                     require_once('composant/itineraire/ajout/ihm/demande_creation.php'); 
                 }  
             
