5916a9818826a36503cd7006b836105438ac77f5
thierry elia <eliamaisongo055@gmail.com>
2023-02-23 10:19:49 +0100

    gestion appareil

---
+++ home/composant/appareil/supprimer/ihm/afficher_users.php
@@ -0,0 +1,102 @@
+<hr />
+<ol class="breadcrumb bc-3" >
+	<li>
+		<a href="index.html"><i class="fa-home"></i>Home</a>
+	</li>
+	<li>
+		<a href="ui-panels.html">Bus</a>
+	</li>
+	<li class="active">
+		<strong>Supprimer</strong>
+	</li>
+</ol>
+
+
+								
+		
+		<br />
+		
+		<script type="text/javascript">
+		jQuery( document ).ready( function( $ ) {
+			var $table1 = jQuery( '#table-1' );
+			
+			// Initialize DataTable
+			$table1.DataTable( {
+				"aLengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+				"bStateSave": true
+			});
+			
+			// Initalize Select Dropdown after DataTables is created
+			$table1.closest( '.dataTables_wrapper' ).find( 'select' ).select2( {
+				minimumResultsForSearch: -1
+			});
+		} );
+		</script>
+		
+		<table class="table table-bordered datatable" id="table-1">
+			<thead>
+				<tr>
+				    <TH></TH>
+					
+					<th>Matricule</th>
+					<th>Marque</th>
+					<th>Total place</th>
+					<th>Action</th>
+	
+				</tr>
+			</thead>
+			<tbody>
+				
+			<?php
+							
+			for($i=0; $i < count($buss); $i++)
+				{         
+					echo"                         
+               			 <tr class='odd gradeX'>
+								<td>$i</td>
+				
+					<td>". $buss[$i]->matricule ."</td>
+					<td > " . $buss[$i]->marque ."</td>
+					<td >". $buss[$i]->total_place."</td>
+					<td style='text-align:center;'><a href='?page=supprimer_bus&id=". $buss[$i]->id ."'><button type='button' class='btn btn-red'>Supprimer</button></a></td>
+					
+					
+		
+				</tr>";
+						         
+				}
+				?>
+		</table>
+		
+		<br />
+		
+	
+
+		
+	
+			
+		
+	
+
+
+
+
+
+	<!-- Imported styles on this page -->
+	<link rel="stylesheet" href="assets/js/datatables/datatables.css">
+	<link rel="stylesheet" href="assets/js/select2/select2-bootstrap.css">
+	<link rel="stylesheet" href="assets/js/select2/select2.css">
+
+	<!-- Bottom scripts (common) -->
+	<script src="assets/js/gsap/TweenMax.min.js"></script>
+	<script src="assets/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+	<script src="assets/js/joinable.js"></script>
+	<script src="assets/js/resizeable.js"></script>
+	<script src="assets/js/neon-api.js"></script>
+
+
+	<!-- Imported scripts on this page -->
+	<script src="assets/js/datatables/datatables.js"></script>
+	<script src="assets/js/select2/select2.min.js"></script>
+	<script src="assets/js/neon-chat.js"></script>
---
+++ home/composant/appareil/supprimer/ihm/form_user.php
@@ -0,0 +1,52 @@
+<hr />
+<ol class="breadcrumb bc-3" >
+	<li>
+		<a href="index.html"><i class="fa-home"></i>Home</a>
+	</li>
+	<li>
+		<a href="ui-panels.html">Bus</a>
+	</li>
+	<li class="active">
+		<strong>Supprimer</strong>
+	</li>
+</ol>
+
+
+<div class="row">
+
+<div class="col-md-12">
+	<div class="alert alert-danger"><strong>Voulez-vous supprimer le bus ci-dessous ? .</strong> <a href='?page=supprimer_bus'><button type='button' class='btn btn-blue'>NON</button></a> <a href='?demande=supprimer_agent&id=<?php echo $id;?>'><button type='button' class='btn btn-red'>OUI</button></a></div>
+</div>
+
+</div>
+
+
+<br />
+		
+		<div class="row">
+			<div class="col-md-12">
+				
+				<table class="table table-bordered responsive">
+					<thead>
+						<tr>
+						 	<th width='15%'>ID</th>
+							<th>Matricule</th>
+							<th>Marque</th>
+							<th>Total de place</th>
+							
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<?php
+							
+								
+										echo"<tr><td>". $id." </td><td>". $matricule ." </td><td> " . $marque ." </td><td> ". $total_place." </td><tr>";
+								
+							?>
+						</tr>				
+					</tbody>
+				</table>
+				
+			</div>
+		</div>
